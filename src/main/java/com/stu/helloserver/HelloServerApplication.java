feeec029bd9fd94f9a2c8cfb7f9425207c4280fb
12858 <1285891570@qq.com>
2026-03-18 00:00:31 +0800

    第二次实验

--- src/main/java/com/stu/helloserver/HelloServerApplication.java
+++ src/main/java/com/stu/helloserver/HelloServerApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-// 关键：添加 ComponentScan 扫描 controller 和 entity 包
+
 @ComponentScan(basePackages = {"com.stu.helloserver", "controller", "entity"})
 @SpringBootApplication
 public class HelloServerApplication {
